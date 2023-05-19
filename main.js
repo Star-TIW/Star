8290575178006826088106483d8dc9f93a76095b
Star <125516384+Nebula-Gamez@users.noreply.github.com>
2023-05-19 21:28:19 +0000

    Update main.js

--- main.js
+++ main.js
@@ -139,8 +139,8 @@ const particleOptions = {
   interactivity: {
     detect_on: "canvas",
     events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: true, mode: "push" },
+      onhover: { enable: false, mode: "repulse" },
+      onclick: { enable: false, mode: "push" },
       resize: true
     },
     modes: {
