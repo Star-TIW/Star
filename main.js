003232d24586da0059ac4ad2eb59e94e33905057
Star <125516384+Nebula-Gamez@users.noreply.github.com>
2023-05-19 21:12:26 +0000

    Update main.js

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ const particleOptions = {
   interactivity: {
     detect_on: "canvas",
     events: {
-      onhover: { enable: true, mode: "repulse" },
+      onhover: { enable: true, mode: "grab" },
       onclick: { enable: true, mode: "push" },
       resize: true
     },
@@ -147,7 +147,7 @@ const particleOptions = {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
       repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 400 },
+      push: { particles_nb: 10 },
       remove: { particles_nb: 2 }
     }
   },
