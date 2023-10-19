f1b4cb315957a67f4edda07c80270f6780470cbb
Star <125516384+Star-TIW@users.noreply.github.com>
2023-10-19 07:55:31 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/star/',
-    bare: 'https://fireworks.sprk.lol/bare/',
+    bare: 'https://banking.norushcharge.com/outerspace/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
