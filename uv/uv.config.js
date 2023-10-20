57a6d0b38ac70571f2dbe4f0aaabe5d73f5f1d97
Star <125516384+Star-TIW@users.noreply.github.com>
2023-10-20 15:56:30 +0000

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/star/',
-    bare: 'https://sushi-history.kawa-kun.com/outerspace/',
+    bare: 'https://fireworks.sprk.lol/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
