5b43a15af0e469382d3ed38c35d2818864585048
Star <125516384+Star-TIW@users.noreply.github.com>
2023-11-27 17:18:01 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/star/',
-    bare: 'https://bettr.vercel.app/outerspace/',
+    bare: 'https://bettr-frfr.vercel.app/outerspace/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
