63e74671ce307ad8f9f18b82fd0d0c9dc3acff37
Star <125516384+Star-TIW@users.noreply.github.com>
2023-12-15 11:31:02 -0800

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/star/',
-    bare: 'https://multiplication.cheap-car-rental.com/bare/',
+    bare: 'https://intellect.organization.tomahawkchurch.org/outerspace/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
