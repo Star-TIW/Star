375befdbeccb5ff809cc8a8a7776fe7c15c0f287
Star <125516384+Star-TIW@users.noreply.github.com>
2024-01-26 18:16:14 -0800

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/star/',
-    bare: 'https://multiplication.cheap-car-rental.com/bare/',
+    bare: 'https:e.d.2of1.org/v/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
