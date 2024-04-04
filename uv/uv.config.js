fdb416fd4f828a9bebc0b47e78506a19cd16ded1
Star <125516384+Star-TIW@users.noreply.github.com>
2024-04-03 17:43:26 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: "/star/",
-    bare: "https://greg.nimali.net/bare/",
+    bare: "https://morepoetryat.gurdit.com/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
