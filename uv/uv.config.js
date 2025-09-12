b2d346cb5b8cd15efe211f310845fe2089f78242
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-11 19:23:05 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: "/star/",
-    bare: "https://https://chaotic-guest-bestie.glacialpower.com/bare/",
+    bare: "https://chaotic-guest-bestie.glacialpower.com/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
