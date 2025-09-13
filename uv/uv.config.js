f84a2f7f9ef7db4d25d28e0b88e2004494c4b840
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-12 23:26:51 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: "/star/",
+    prefix: "./star/",
     bare: "https://55gms.com/t/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: './uv/uv.handler.js',
+    bundle: './uv/uv.bundle.js',
+    config: './uv/uv.config.js',
+    sw: './uv/uv.sw.js',
 };
